c46c805ed194d64e9b82eb1e75d2c04699453849
Alwandy Harold Kalangi <aharoldk@gmail.com>
2019-08-19 14:03:00 +0700

    Remove unused file

--- android/mysdk/src/main/java/com/aharoldk/mysdk/MySDKActivity.java
+++ android/mysdk/src/main/java/com/aharoldk/mysdk/MySDKActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.facebook.react.BuildConfig;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactRootView;
 import com.facebook.react.common.LifecycleState;
